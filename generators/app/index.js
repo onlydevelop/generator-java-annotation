79bd13abf17a304ac3d96e5223ba956555726282
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-01-30 23:12:01 +0530

    Added the prompting

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,48 @@
 'use strict';
+
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
+
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the swell ${chalk.red('generator-java-annotation')} generator!`)
     );
 
-    const prompts = [
+    this.answers = await this.prompt([
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type    : 'input',
+        name    : 'projectName',
+        message : 'Project name',
+        default : 'example'
+      },
+      {
+        type    : 'input',
+        name    : 'packageName',
+        message : 'Package Name',
+        default : 'com.example'
+      },
+      {
+        type    : 'input',
+        name    : 'javaVersion',
+        message : 'Java Version',
+        default : '1.8'
+      },
+      {
+        type    : 'input',
+        name    : 'gradleVersion',
+        message : 'Gradle Version',
+        default : '5.1'
+      },
+      {
+        type    : 'input',
+        name    : 'junitVersion',
+        message : 'JUnit Version',
+        default : '4.12'
       }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
-  install() {
-    this.installDependencies();
+    ]);
   }
 };
